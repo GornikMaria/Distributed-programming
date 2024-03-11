999667dec17f697a0c36ff36b33ecd43f4307544
Mariaps11 <beyordscene@gmail.com>
2024-03-11 17:14:34 +0300

    исправления

--- Valuator/IStorage.cs
+++
@@ -1,9 +0,0 @@
-namespace Valudator
-{
-    public interface IStorage
-    {
-        void Store(string key, string value);
-        string Load(string key);
-        void SimilarityStore(string similarityKey, string text);
-    }
-}
